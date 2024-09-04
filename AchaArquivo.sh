a8dc22f599b5c6ed42716160e3a50ee078add2c2
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-09-04 09:03:15 -0300

    Update AchaArquivo.sh

--- AchaArquivo.sh
+++ AchaArquivo.sh
@@ -15,10 +15,12 @@
 ## Exemplo de uso: /home/matheus.sivano/AchaArquivo.sh                               ##
 ##                                                                                   ##
 ##                                                                                   ##
+## - Edição, 04/09: Adicão das CP /home/skyline/SODEXHOPASS.BANCOS/sent              ##
+## /home/skyline/PRODUCAO.QUARENTENA/sent                                            ##
 #######################################################################################
 
 read -p "Insira a nomenclatura: " NomeArquivo
 
-Saida= find /home/skyline/OPERADORAS.SEMRELACIONAMENTO/sent /home/skyline/.QUARENTENA/mailbox /home/skyline/CIELO.SEMPV/sent /home/skyline/ALELO.SEMPV/sent /home/skyline/POLICARD.SEMRELACIONAMENTO /home/skyline/TICKET.SEMRELACIONAMENTO /home/skyline/SANTANDER.SEMRELACIONAMENTO/sent -type f -iname '*'$NomeArquivo'*' -exec ls -ltr {} \;
+Saida= find /home/skyline/OPERADORAS.SEMRELACIONAMENTO/sent /home/skyline/.QUARENTENA/mailbox /home/skyline/CIELO.SEMPV/sent /home/skyline/ALELO.SEMPV/sent /home/skyline/POLICARD.SEMRELACIONAMENTO /home/skyline/TICKET.SEMRELACIONAMENTO /home/skyline/SANTANDER.SEMRELACIONAMENTO/sent /home/skyline/SODEXHOPASS.BANCOS/sent /home/skyline/PRODUCAO.QUARENTENA/sent -type f -iname '*'$NomeArquivo'*' -exec ls -ltr {} \;
 
 echo $Saida
